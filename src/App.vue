7a09abb152bb12b54e3fd1db44115422d6ae3178
Skylinker <skylinkertu33@gmail.com>
2022-06-29 19:59:41 +0700

    Add files

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: 'Example'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+html, body {
+  margin: 0;
+  height: 100vh;
+}
+</style>
